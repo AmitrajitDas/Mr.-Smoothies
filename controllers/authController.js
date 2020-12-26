f5a4c6503c24bcaffcafe13ad6b0bb3ca054b9d5
Amitrajit31 <amitrajitdas31@gmail.com>
2020-12-26 16:06:03 +0530

    body parsed

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,13 @@ module.exports.login_get = (req,res) => {
 }
 
 module.exports.signup_post = (req,res) => {
+    const {email, password} = req.body;
+    console.log(email, password);
     res.send('new signup');
 }
 
 module.exports.login_post = (req,res) => {
+    const {email, password} = req.body;
+    console.log(email, password);
     res.send('user login');
 }
