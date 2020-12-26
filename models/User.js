0a32c5e71730536782b057fc9b33ef4d33399c32
Amitrajit31 <amitrajitdas31@gmail.com>
2020-12-26 19:45:18 +0530

    mongoose model validated

--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: 'string',
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email'] 
     },
 
     password: {
         type: 'string',
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter an password'],
+        minlength: [6, 'Minimum Password length is 6 characters']
     },
 });
 
