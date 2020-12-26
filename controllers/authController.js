163a74cc51f69a7ca138ad8259356f1cab691b08
Amitrajit31 <amitrajitdas31@gmail.com>
2020-12-26 16:53:30 +0530

    user created using user model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req,res) => {
     res.render('signup');
 }
@@ -6,14 +8,23 @@ module.exports.login_get = (req,res) => {
     res.render('login');
 }
 
-module.exports.signup_post = (req,res) => {
+module.exports.signup_post = async (req,res) => {
     const {email, password} = req.body;
-    console.log(email, password);
-    res.send('new signup');
+
+     try{
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('error! user not created');
+    }
+    
 }
 
 module.exports.login_post = (req,res) => {
     const {email, password} = req.body;
+    
     console.log(email, password);
     res.send('user login');
 }
