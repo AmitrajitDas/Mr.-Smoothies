fbcd4972ddea14ea816f9a63e36fff143576d2d0
Amitrajit31 <amitrajitdas31@gmail.com>
2020-12-29 21:43:12 +0530

    revoked cookies to use jwt

--- app.js
+++ app.js
@@ -24,25 +24,3 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-//Cookies
-
-app.get('/set-cookies', (req, res) =>{
-
-    //res.setHeader('Set-Cookie', 'newUser=true');
-
-    res.cookie('newUser', false);
-    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-
-    res.send("you got the cookies");
-
-});
-
-app.get('/read-cookies', (req, res) =>{
-
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-
-  res.json(cookies);
-
-});
-
