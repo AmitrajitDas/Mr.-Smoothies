f4501ee59644d03e3205c55908324ce91d422b08
Amitrajit31 <amitrajitdas31@gmail.com>
2020-12-29 21:44:38 +0530

    crated jwt tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //Handle Errors
 const handleErrors = (err) => {
@@ -22,6 +23,13 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'my secret', {
+        expiresIn: maxAge
+    });
+}
+
 module.exports.signup_get = (req,res) => {
     res.render('signup');
 }
@@ -35,7 +43,9 @@ module.exports.signup_post = async (req,res) => {
 
      try{
         const user = await User.create({email, password});
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({user: user._id});
     }
     catch(err){
         const errors = handleErrors(err);
