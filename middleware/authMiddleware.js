d280880d7ab19d5a432a62819aa91888326d6c87
Amitrajit31 <amitrajitdas31@gmail.com>
2020-12-31 16:02:08 +0530

    added middleware to check the current user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,4 +19,24 @@ const requireAuth = (req, res, next) => {
   }
 };
 
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'my secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
 module.exports = { requireAuth };
